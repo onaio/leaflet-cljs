a05a68e7bd632e79c5ae1befad6aeb08b306726a
Roy Rutto <royrutto@gmail.com>
2015-08-28 16:33:19 +0300

    RR: add getZoom to externs in cljs packaging

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject prabhasp/leaflet-cljs "0.7.3"
- :description "osmtogeojson"
+(defproject onaio/leaflet-cljs "0.7.3-SNAPSHOT"
+ :description "leaflet-cljs packaging"
   :url "https://leafletjs.com"
    :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"})
